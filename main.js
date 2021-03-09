b76372f972b1e60336daa8fac8ad7491d6661128
ernesto <https://github.com/TxC-Toxic/BOT-BY-SATAN.git>
2021-03-09 20:17:45 +0100

    botV2

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = 50373488366','50373488366','50363488366'] // Put your number here
+global.owner = ['50373488366'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
